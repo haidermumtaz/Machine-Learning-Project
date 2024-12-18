1f56d48ed47a1f51af2ba7d7bb760144b52d5146
Haider Mumtaz <haidermumtaz98@gmail.com>
2024-12-17 21:38:03 -0500

    EDA and Data Cleaning

--- eda_utils.py
+++ eda_utils.py
@@ -1,5 +1,5 @@
 # eda_utils.py
-# I was creating multiple correlation matrix heatmaps so I decided to just create a function to do it lol. I'll add more functions here as I need them.
+# I was creating multiple correlation matrix heatmaps so I decided to just create a function to do it hehehe. I'll add more functions here as I need them.
 
 import numpy as np
 import pandas as pd
@@ -7,14 +7,15 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 
-def plot_corr_matrix(df, nr_c, targ):
+def plot_corr_matrix(df, targ, nr_c=None):
     """
-    Plots a correlation matrix heatmap for the top 'nr_c' features most correlated with 'targ' in dataframe 'df'.
+    Plots a correlation matrix heatmap. If nr_c is specified, shows top correlations with target.
+    Otherwise shows correlations for all columns with target.
     
     Parameters:
         df (pd.DataFrame): The DataFrame containing the data.
-        nr_c (int): Number of top correlated features to include.
         targ (str): The target column for correlation.
+        nr_c (int, optional): Number of top correlated features to include.
         
     Returns:
         None
@@ -22,23 +23,36 @@ def plot_corr_matrix(df, nr_c, targ):
     # Compute the absolute correlation matrix
     corr_abs = df.corr().abs()
     
-    # Select the top 'nr_c' features correlated with 'targ'
-    top_features = corr_abs.nlargest(nr_c, targ)[targ].index
-    
-    # Compute the correlation matrix for the selected features
-    cm = df[top_features].corr()
+    # If nr_c is specified, select top correlated features
+    if nr_c is not None:
+        top_features = corr_abs.nlargest(nr_c, targ)[targ].index
+        cm = df[top_features].corr()
+        title = f'Correlation Matrix - Top {nr_c} Features Correlated with {targ}'
+        figsize = (nr_c / 1.5, nr_c / 1.5)
+        annot_size = 10
+        cbar_kws = {'shrink': .5}  # Smaller colorbar for fewer features
+    else:
+        cm = corr_abs
+        title = f'Correlation Matrix - All Features with {targ}'
+        figsize = (20, 16)
+        annot_size = 7
+        cbar_kws = {'shrink': .95}  # Larger colorbar for many features
     
     # Plot the heatmap
-    plt.figure(figsize=(nr_c / 1.5, nr_c / 1.5))
+    plt.figure(figsize=figsize)
     sns.set(font_scale=1.25)
     sns.heatmap(
         cm,
-        linewidths=1.5,
-        annot=True,
-        square=True,
-        fmt='.2f',
-        annot_kws={'size': 10},
-        cmap="Blues"
+        linewidths = 0.5,
+        annot = True,
+        square = True,
+        fmt = '.2f',
+        annot_kws = {'size': annot_size},
+        cmap = "Blues",
+        cbar_kws = cbar_kws  # Add colorbar customization
     )
-    plt.title(f'Correlation Matrix - Top {nr_c} Features Correlated with {targ}')
+    plt.title(title)
+    plt.xticks(rotation=45, ha='right')
+    plt.yticks(rotation=0)
+    plt.tight_layout()
     plt.show()
